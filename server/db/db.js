3ac02a3d24f71f49d3fbe9cb06280fe77769e7fd
Ankit Kumar <ankitk98165@gmail.com>
2026-02-23 22:29:35 +0530

    first commit

---
+++ server/db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+exports.connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    console.log("db connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const {connectDB} = require("./db/db")
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on  http://localhost:${port}`)
+})
