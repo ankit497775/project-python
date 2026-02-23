511ca56599b0224e6df080d12b0fc8f62b38d0e2
Ankit Kumar <ankitk98165@gmail.com>
2026-02-23 22:32:15 +0530

    second msg

--- server/index.js
+++ server/index.js
@@ -11,4 +11,12 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`App listening on  http://localhost:${port}`)
+  console.log(`App listening on  http://localhost:${port}`)
+  console.log(`App listening on  http://localhost:${port}`)
+  console.log(`App listening on  http://localhost:${port}`)
+  console.log(`App listening on  http://localhost:${port}`)
+  console.log(`App listening on  http://localhost:${port}`)
+  console.log(`App listening on  http://localhost:${port}`)
+  console.log(`App listening on  http://localhost:${port}`)
+  console.log(`App listening on  http://localhost:${port}`)
 })
